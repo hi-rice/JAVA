5f145c95b664c90237b3da95cda5e790872ec8e1
권혜연 <rice@gwonhyeyeon-ui-MacBookAir.local>
2024-08-15 15:39:03 +0900

    java-start

---
+++ src/variable/Var7.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 100; // 정수 리터럴
+        double b = 10.5; // 실수 리터럴
+        boolean c = true; // 불리언(boolean) true, false 만 입력 가능, 불리언 리터럴
+        char d = 'A'; // 문자 하나 리터럴
+        String e = "Hello  Java"; // 문자열 리터럴, 문자열을 다르기 위한 특별한 타입
+
+        //int z = "백원"; // 타입이 안 맞음.
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
